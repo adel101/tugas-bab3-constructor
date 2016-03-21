9c152dcf890c4e6f18fd96a5f6b0c1f5246526db
Adel Syafrizal Pratama <ngekngok101@gmail.com>
2016-03-21 21:32:23 +0700

    Data karyawan

---
+++ zzzz/src/Karyawan.java
@@ -0,0 +1,30 @@
+
+public class Karyawan {
+
+    private String[] Nama = {"Paijo", "Lala", "Lulu", "Sabar", "Sule", "Paiman", "Ponimin", "Sutiman", "Raja", "Paijo"};
+
+    private int[][] DataGAJI = {
+        {101, 2000, 1000, 500, 200},
+        {102, 1750, 900, 500, 200},
+        {103, 2150, 1000, 300, 375},
+        {104, 1500, 750, 600, 200},
+        {105, 2500, 850, 500, 300},
+        {106, 1500, 350, 450, 250},
+        {107, 2000, 1000, 500, 200},
+        {108, 1500, 1250, 500, 300},
+        {109, 1000, 1000, 500, 200},
+        {110, 2250, 1000, 200, 200}
+    };
+
+    public void Data() {
+        System.out.println("\tData Karyawan");
+        System.out.println("");
+        System.out.printf("%s %9s %17s %15s %12s %15s \n",
+                "Nik", "Nama", "Gaji Pokok", "Tunjangan", "Bonus", "Denda");
+
+        {
+            
+            
+        
+    
+{
